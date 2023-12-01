1d01578297729ae0151efc613b4f95b4ae1d9c2e
Danny Yoo <dannyyoo@google.com>
2023-12-01 11:22:14 -0800

    Getting part 1 done.

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+fn part_1(s: &str) -> i32 {
+    s.lines()
+        .map(|l| {
+            let s = l.chars().filter(char::is_ascii_digit).collect::<String>();
+            let first_digit = s.chars().next().unwrap_or('0');
+            let last_digit = s.chars().last().unwrap_or('0');
+            println!("{}, {}, {}", l, first_digit, last_digit);
+            10 * (first_digit as i32 - '0' as i32) + (last_digit as i32 - '0' as i32)
+        })
+        .sum()
+}
+
+#[test]
+fn test_part_1() {
+    assert_eq!(
+        part_1(
+            "\
+	1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+	"
+        ),
+        142
+    )
+}
+
+fn main() {
+    println!("Hello, world!");
+}
