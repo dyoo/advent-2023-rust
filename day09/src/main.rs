42cf572da45987f38ba69dab38338a6d9dee5d2d
Danny Yoo <dannyyoo@google.com>
2023-12-09 13:51:08 -0800

    Day 9 part 2 complete.
    Huh.  This was a bit trivially easy.  Maybe I'll have time to work on
    earlier days now.

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -10,12 +10,12 @@ fn parse_histories(lines: &str) -> Vec<Vec<Num>> {
     lines.lines().map(parse_history_line).collect()
 }
 
-fn predict(vals: &[Num]) -> Option<Num> {
+fn predict_last(vals: &[Num]) -> Option<Num> {
     let diffs = differences(vals);
     if all_same(diffs.iter().copied()) {
         Some(vals.last()? + diffs.last()?)
     } else {
-        let next = predict(&diffs)?;
+        let next = predict_last(&diffs)?;
         Some(vals.last()? + next)
     }
 }
@@ -48,20 +48,49 @@ fn test_all_same() {
 }
 
 #[test]
-fn test_predict() {
-    assert_eq!(predict(&[]), None);
-    assert_eq!(predict(&[1]), None);
-    assert_eq!(predict(&[0, 0]), Some(0));
-    assert_eq!(predict(&[0, 2, 4, 6]), Some(8));
-    assert_eq!(predict(&[3, 3, 5, 9, 15]), Some(23));
-    assert_eq!(predict(&[1, 3, 6, 10, 15, 21]), Some(28));
+fn test_predict_last() {
+    assert_eq!(predict_last(&[]), None);
+    assert_eq!(predict_last(&[1]), None);
+    assert_eq!(predict_last(&[0, 0]), Some(0));
+    assert_eq!(predict_last(&[0, 2, 4, 6]), Some(8));
+    assert_eq!(predict_last(&[3, 3, 5, 9, 15]), Some(23));
+    assert_eq!(predict_last(&[1, 3, 6, 10, 15, 21]), Some(28));
+}
+
+fn predict_first(vals: &[Num]) -> Option<Num> {
+    let diffs = differences(vals);
+    if all_same(diffs.iter().copied()) {
+        Some(vals.first()? - diffs.last()?)
+    } else {
+        let diff = predict_first(&diffs)?;
+        Some(vals.first()? - diff)
+    }
+}
+
+#[test]
+fn test_predict_first() {
+    assert_eq!(predict_first(&[]), None);
+    assert_eq!(predict_first(&[1]), None);
+    assert_eq!(predict_first(&[0, 0]), Some(0));
+    assert_eq!(predict_first(&[2, 2, 2]), Some(2));
+    assert_eq!(predict_first(&[0, 2, 4, 6]), Some(-2));
+    assert_eq!(predict_first(&[3, 3, 5, 9, 15]), Some(5));
+    assert_eq!(predict_first(&[10, 13, 16, 21, 30, 45]), Some(5));
 }
 
 fn part_1(s: &str) -> Num {
     let histories = parse_histories(s);
     histories
         .into_iter()
-        .filter_map(|history| predict(&history))
+        .filter_map(|history| predict_last(&history))
+        .sum()
+}
+
+fn part_2(s: &str) -> Num {
+    let histories = parse_histories(s);
+    histories
+        .into_iter()
+        .filter_map(|history| predict_first(&history))
         .sum()
 }
 
@@ -79,4 +108,5 @@ fn main() {
     let input = std::fs::read_to_string("input.txt").expect("input file");
 
     println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
 }
