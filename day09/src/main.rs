4574c4dde351ffbbc830381a86298746d9ebcfda
Danny Yoo <dannyyoo@google.com>
2023-12-09 13:42:58 -0800

    Initial commit for day 9, part 1.
    Minor: we needed to use i64s to pass this, as otherwise overflow could occur.

---
+++ day09/src/main.rs
@@ -0,0 +1,82 @@
+type Num = i64;
+
+fn parse_history_line(line: &str) -> Vec<Num> {
+    line.split_whitespace()
+        .filter_map(|n| n.parse::<Num>().ok())
+        .collect()
+}
+
+fn parse_histories(lines: &str) -> Vec<Vec<Num>> {
+    lines.lines().map(parse_history_line).collect()
+}
+
+fn predict(vals: &[Num]) -> Option<Num> {
+    let diffs = differences(vals);
+    if all_same(diffs.iter().copied()) {
+        Some(vals.last()? + diffs.last()?)
+    } else {
+        let next = predict(&diffs)?;
+        Some(vals.last()? + next)
+    }
+}
+
+fn differences(vals: &[Num]) -> Vec<Num> {
+    vals.windows(2)
+        .filter_map(|window| match window {
+            [x, y] => Some(y - x),
+            _ => None,
+        })
+        .collect()
+}
+
+fn all_same(vals: impl IntoIterator<Item = Num>) -> bool {
+    let mut items = vals.into_iter();
+    if let Some(first) = items.next() {
+        items.all(|item| item == first)
+    } else {
+        true
+    }
+}
+
+#[test]
+fn test_all_same() {
+    assert!(all_same([]));
+    assert!(all_same([3]));
+    assert!(all_same([3, 3]));
+
+    assert!(!all_same([3, 4]));
+}
+
+#[test]
+fn test_predict() {
+    assert_eq!(predict(&[]), None);
+    assert_eq!(predict(&[1]), None);
+    assert_eq!(predict(&[0, 0]), Some(0));
+    assert_eq!(predict(&[0, 2, 4, 6]), Some(8));
+    assert_eq!(predict(&[3, 3, 5, 9, 15]), Some(23));
+    assert_eq!(predict(&[1, 3, 6, 10, 15, 21]), Some(28));
+}
+
+fn part_1(s: &str) -> Num {
+    let histories = parse_histories(s);
+    histories
+        .into_iter()
+        .filter_map(|history| predict(&history))
+        .sum()
+}
+
+#[test]
+fn test_part_1() {
+    let input = "\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+";
+    assert_eq!(part_1(input), 114);
+}
+
+fn main() {
+    let input = std::fs::read_to_string("input.txt").expect("input file");
+
+    println!("Part 1: {}", part_1(&input));
+}
